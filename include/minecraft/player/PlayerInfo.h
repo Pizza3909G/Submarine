db3b5fbe1197285b2a9e09cfe234801118ff492b
Nayukikuu <80696477+Pizza3909G@users.noreply.github.com>
2024-05-19 19:26:57 +0700

    .

---
+++ include/minecraft/player/PlayerInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+class PlayerInfo {
+    public:
+    PlayerInfo(std::string username);
+    std::string getUsername();
+}
---
+++ src/minecraft/player/PlayerInfo.cpp
@@ -0,0 +1,2 @@
+#include "minecraft/player/PlayerInfo.cpp"
+
